c82663ff308903b0bc37d284b1537d023259af6c
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-09-30 13:05:50 +0530

    initial deploy

--- maboi.py
+++ maboi.py
@@ -1,14 +1,32 @@
 import requests   
 import webbrowser
 import bs4
-res = requests.get('https://docs.google.com/forms/d/e/1FAIpQLSdmUGSzhsbT_w4x34DyVoOvCcnMsrWEjZwM_MCqIDWgC_tAAg/viewform?usp=sf_link') 
-noStarchSoup = bs4.BeautifulSoup(res.text,"html.parser")                                  
+from googlesearch import search
+link=input("Enter the google form URL here:\n")
+res = requests.get(link) 
+noStarchSoup = bs4.BeautifulSoup(res.text,"html.parser")   
+heading=noStarchSoup.select('div .freebirdFormviewerViewHeaderTitle')
+file1 = open("try.txt","w")
 elems = noStarchSoup.select('div .freebirdFormviewerComponentsQuestionBaseTitle') 
+j=1
+print("------------------------------------------------\n--------------LINKS FOR YOU LAZY----------------")
 for i in elems:
-    res = requests.get('http://google.com/search?q=' + (i.getText()[:-1]))
-    #webbrowser.open('http://google.com/search?q=' + (i.getText()[:-1]))
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text,"html.parser")
-    linkElems = soup.select(".r a")
-    print(len(linkElems))
-    #webbrowser.open('http://google.com' + (linkElems[0].get('href')))
+    print("------------------------------------------------")
+    # desktop user-agent
+    for url in search(i.getText()[:-1], tld='com', stop=1):
+        print("Q",j,")",i.getText()[:-1],"\n\nLink:",url)
+        j=j+1
+print("------------------------------------------------")
+while(True):
+    alternate=input("you might have issue with some quesitons first link, type in the quesiton number for second and third link of it's answers\n")
+    j=0
+    print("Q )",str(elems[int(alternate)-1].getText())[:-1])
+    for url in search(str(elems[int(alternate)-1].getText())[:-1], tld='com', stop=3):
+        if(j!=0):
+            print("\n Link",j+1,":",url)
+        j=j+1
+    print("------------------------------------------------")
+    choice=input("Do you need extra links for any more questions? Enter Y or y as yes and N as NOoooo\n")
+    if(choice!="y" and choice!="Y"):
+        break
+print("::::::::::::::: Thanks For using :::::::::::::")
